bb26e9b652a61809cc3c35ef978472a45879dd78
Nikodem <nikodem.stecko@gmail.com>
2026-03-13 22:14:32 +0100

    Added first machine controllers

---
+++ api/Smart Factory CMMS/Controllers/MachinesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Smart_Factory_CMMS.Data;
+using Smart_Factory_CMMS.Models;
+
+namespace Smart_Factory_CMMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachinesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MachinesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Machine>>> GetMachines()
+        {
+            var machines = await _context.Machines.ToListAsync();
+            return Ok(machines);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Machine>> GetMachineById(Guid id)
+        {
+            var machine = await _context.Machines.FindAsync(id);
+
+            if (machine == null)
+            {
+                return NotFound();
+            }
+            return Ok(machine);
+        }
+    }
+}
